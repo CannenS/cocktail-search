289ab96bd4565593d6e84333351307896a34cd3f
Cannen Schilling <cannen.0schilling@gmail.com>
2022-11-15 19:53:52 -0500

    adding files

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about page</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste
+        blanditiis optio quos, maiores architecto quod saepe voluptas eveniet
+        nulla, numquam sunt accusantium et perspiciatis veniam placeat harum?
+        Voluptatem, numquam delectus. Reprehenderit, possimus. Maiores enim
+        cumque error dicta exercitationem? Tempore, eius!
+      </p>
+    </section>
+  );
+};
+
+export default About;
